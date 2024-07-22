d61322ccd01fb8a1b4f6ce30dc238ac93267225c
cryogon <jtkalga@gmail.com>
2024-07-22 22:52:04 +0530

    feat: store data in version

--- index.ts
+++ index.ts
@@ -1,12 +1,15 @@
 import config from "./config.json";
 import fs from "node:fs";
-import { join, dirname } from "node:path";
+import { join, dirname, normalize } from "node:path";
 import { format } from "./utils/format";
 import { uploadFile } from "./drive";
 
 const osuFolder = config.osu_folder_path || "";
 const filesToTrack = config.files_to_track || [];
 const destinationPath = config.copy_to || "";
+const unoveritableFiles = (config.save_in_versions || []).map((file) =>
+  normalize(file)
+);
 // let latestTaskId = "";
 
 for (const fileName of filesToTrack) {
@@ -38,24 +41,45 @@ for (const fileName of filesToTrack) {
         fs.mkdirSync(dirname(destPath), { recursive: true });
       }
 
-      fs.copyFileSync(srcPath, destPath, fs.constants.COPYFILE_FICLONE);
+      if (fs.readFileSync(srcPath, "utf8").length) {
+        fs.copyFileSync(srcPath, destPath, fs.constants.COPYFILE_FICLONE);
+      }
+
+      if (unoveritableFiles.includes(normalize(fileName))) {
+        const versionFolder = join(destinationPath, "versions");
+        if (!fs.existsSync(versionFolder)) {
+          fs.mkdirSync(versionFolder, { recursive: true });
+        }
+        const ver = fs.readdirSync(versionFolder).length;
+        const verFileName = `${join(
+          versionFolder,
+          fileName.split(".")[0]
+        )}_v${ver}.${fileName.split(".")[1]}`;
+        fs.copyFileSync(srcPath, verFileName, fs.constants.COPYFILE_FICLONE);
+      }
       if (
         config.google_drive_parent_folder_id &&
         config.google_service_account
       ) {
+        // upload to drive
         // FIXME: Some issue in queue where task is pausing indefinately for some reason
         // latestTaskId = loadTask(
         //   uploadFile,
         //   srcPath,
         //   config.google_drive_parent_folder_id
         // );
-
         uploadFile(srcPath, config.google_drive_parent_folder_id)
           .then(() => {
-            console.log("uploaded", srcPath);
+            fs.appendFileSync(
+              "logs.txt",
+              format("uploaded", srcPath, "to drive")
+            );
           })
           .catch((err) => {
-            console.error("failed to upload:", srcPath, "err: ", err);
+            fs.appendFileSync(
+              "logs.txt",
+              format("failed to upload:", srcPath, "err: ", err)
+            );
           });
       }
 
